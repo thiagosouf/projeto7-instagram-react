98e1c8e28e95ba77c4c9dfba2d6bff95cac7171b
thiagosouf <thiagosouf@gmail.com>
2022-03-11 12:48:28 -0300

    componente do Sugestao

--- src/Corpo.js
+++ src/Corpo.js
@@ -1,5 +1,6 @@
 import Story from "./Story"
 import Post from "./Post"
+import Sugestao from "./Sugestao"
 
 export default function Corpo() {
     return (
@@ -42,66 +43,11 @@ export default function Corpo() {
                         Sugestões para você
                         <div>Ver tudo</div>
                     </div>
-                    {/* <Sugestao /> */}
-                    <div class="sugestao">
-                        <div class="usuario">
-                            <img src="assets/img/bad.vibes.memes.svg" />
-                            <div class="texto">
-                                <div class="nome">bad.vibes.memes</div>
-                                <div class="razao">Segue você</div>
-                            </div>
-                        </div>
-
-                        <div class="seguir">Seguir</div>
-                    </div>
-
-                    <div class="sugestao">
-                        <div class="usuario">
-                            <img src="assets/img/chibirdart.svg" />
-                            <div class="texto">
-                                <div class="nome">chibirdart</div>
-                                <div class="razao">Segue você</div>
-                            </div>
-                        </div>
-
-                        <div class="seguir">Seguir</div>
-                    </div>
-
-                    <div class="sugestao">
-                        <div class="usuario">
-                            <img src="assets/img/razoesparaacreditar.svg" />
-                            <div class="texto">
-                                <div class="nome">razoesparaacreditar</div>
-                                <div class="razao">Novo no Instagram</div>
-                            </div>
-                        </div>
-
-                        <div class="seguir">Seguir</div>
-                    </div>
-
-                    <div class="sugestao">
-                        <div class="usuario">
-                            <img src="assets/img/adorable_animals.svg" />
-                            <div class="texto">
-                                <div class="nome">adorable_animals</div>
-                                <div class="razao">Segue você</div>
-                            </div>
-                        </div>
-
-                        <div class="seguir">Seguir</div>
-                    </div>
-
-                    <div class="sugestao">
-                        <div class="usuario">
-                            <img src="assets/img/smallcutecats.svg" />
-                            <div class="texto">
-                                <div class="nome">smallcutecats</div>
-                                <div class="razao">Segue você</div>
-                            </div>
-                        </div>
-
-                        <div class="seguir">Seguir</div>
-                    </div>
+                    <Sugestao img="assets/img/bad.vibes.memes.svg" nome="bad.vibes.memes" razao="Segue você"/>
+                    <Sugestao img="assets/img/chibirdart.svg" nome="chibirdart" razao="Segue você"/>
+                    <Sugestao img="assets/img/razoesparaacreditar.svg" nome="razoesparaacreditar" razao="Novo no Instagram"/>
+                    <Sugestao img="assets/img/adorable_animals.svg" nome="adorable_animals" razao="Segue você"/>
+                    <Sugestao img="assets/img/smallcutecats.svg" nome="smallcutecats" razao="Segue você"/>
                 </div>
 
                 <div class="links">
