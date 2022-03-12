b3ffdd48391df223940be2efaa9dba0360383877
thiagosouf <thiagosouf@gmail.com>
2022-03-12 05:38:13 -0300

    retirando comentarios

--- src/Esquerda.js
+++ src/Esquerda.js
@@ -6,15 +6,6 @@ export default function Esquerda() {
         <div class="esquerda">
             <div class="stories">
                 <Story />
-                {/* <Story src="assets/img/9gag.svg" usuario="9gag" />
-                <Story src="assets/img/meowed.svg" usuario="meowed" />
-                <Story src="assets/img/barked.svg" usuario="barked" />
-                <Story src="assets/img/nathanwpylestrangeplanet.svg" usuario="nathanwpylestrangeplanet" />
-                <Story src="assets/img/wawawicomics.svg" usuario="wawawicomics" />
-                <Story src="assets/img/respondeai.svg" usuario="respondeai" />
-                <Story src="assets/img/filomoderna.svg" usuario="filomoderna" />
-                <Story src="assets/img/memeriagourmet.svg" usuario="memeriagourmet" /> */}
-
                 <div class="setinha">
                     <ion-icon name="chevron-forward-circle"></ion-icon>
                 </div>
@@ -22,8 +13,6 @@ export default function Esquerda() {
 
             <div class="posts">
                 <Post />
-                {/* <Post src="assets/img/meowed.svg" usuario="meowed" conteudo="assets/img/gato-telefone.svg" curtidaImg="assets/img/respondeai.svg" curtidaNome="respondeai" curtidaQnt="101.523" />
-                <Post src="assets/img/barked.svg" usuario="barked" conteudo="assets/img/dog.svg" curtidaImg="assets/img/adorable_animals.svg" curtidaNome="adorable_animals" curtidaQnt="99.159" /> */}
             </div>
         </div>
     )
--- src/SideBar.js
+++ src/SideBar.js
@@ -5,18 +5,12 @@ export default function SideBar(){
     return(
         <div class="sidebar">
                 <Usuario src="assets/img/catanacomics.svg" nickname="catanacomics" nome="Catana"/>
-
                 <div class="sugestoes">
                     <div class="titulo">
                         Sugestões para você
                         <div>Ver tudo</div>
                     </div>
                     <Sugestao />
-                    {/* <Sugestao img="assets/img/bad.vibes.memes.svg" nome="bad.vibes.memes" razao="Segue você"/>
-                    <Sugestao img="assets/img/chibirdart.svg" nome="chibirdart" razao="Segue você"/>
-                    <Sugestao img="assets/img/razoesparaacreditar.svg" nome="razoesparaacreditar" razao="Novo no Instagram"/>
-                    <Sugestao img="assets/img/adorable_animals.svg" nome="adorable_animals" razao="Segue você"/>
-                    <Sugestao img="assets/img/smallcutecats.svg" nome="smallcutecats" razao="Segue você"/> */}
                 </div>
 
                 <div class="links">
